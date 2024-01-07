bb3b4f19d4fd91f498a5832a54b03121009c3621
Bethany Ingraham <BethFIngraham@gmail.com>
2024-01-07 15:08:14 -0500

    solutions for intro to react sprint challenge

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -8,11 +8,32 @@ const urlPeople = 'http://localhost:9009/api/people'
 function App() {
   // ❗ Create state to hold the data from the API
   // ❗ Create effects to fetch the data and put it in state
+
+   const [people, setPeople] = useState([]); 
+  
+  useEffect(() =>{
+   axios.get(urlPeople)
+   .then(res => {
+    axios.get(urlPlanets)
+    .then(res2 => {
+      const people = res.data;
+      const planets = res2.data;
+      const updatedPeople = people.map(person => {
+        return {...person, planet: planets.find(planet => planet.id === person.homeworld)};
+      }) 
+      setPeople(updatedPeople);
+    })
+   })
+  },[])
+
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
       {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {people.map( person => {
+        return <Character key={person.id} person={person}/>
+      })}  
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function Character() { // ❗ Add the props
+
+function Character({person}) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  
+  const [homeworld, setHomeWorld] = useState(false);
+
   return (
-    <div>
+    <>
+    <div className="character-card" onClick={() => {setHomeWorld(!homeworld)}}>
       {/* Use the same markup with the same attributes as in the mock */}
-    </div>
+      <h3 className="character-name">{person.name}</h3>  
+      {homeworld ? <p>Planet: <span className="character-planet">{person.planet.name}</span></p> : ''}
+    </div> 
+    </>
   )
 }
 
